e236525414250a104a220a380d0b33139302332b
Roberto <betomax_16@live.com.mx>
2022-02-28 15:23:05 -0600

    Creacion de nuevo campo 'print'  para modelo sucursal

--- src/models/sucursal.ts
+++ src/models/sucursal.ts
@@ -4,12 +4,14 @@ export interface ISucursal extends Document {
     name: string;
     color: string;
     timeLimit: number;
+    print: string;
 }
 
 const SucursalSchema: Schema = new Schema({
     name: { type: String, required: true },
     color: { type: String, required: true },
-    timeLimit: { type: Number, required: true }
+    timeLimit: { type: Number, required: true },
+    print: { type: String, required: false }
 },
 { timestamps: true });
 
